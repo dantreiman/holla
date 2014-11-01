df1accb1a744b62e115a8703988d3100af660f01
Dan Treiman <dan.treiman@gmail.com>
2014-11-01 16:16:53 -0700

    Initial commit of empty project

---
+++ Holla Back/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Holla Back
+//
+//  Created by Dan Treiman on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Holla Back/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Holla Back
+//
+//  Created by Dan Treiman on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
