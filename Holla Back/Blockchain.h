311000efb581b91313d50aba01c9d7aa10185c06
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-11-01 16:40:45 -0700

    skeleton blockchain wrapper

---
+++ Holla Back/Blockchain.h
@@ -0,0 +1,15 @@
+//
+//  Blockchain.h
+//  Holla Back
+//
+//  Created by Julian Vergel de Dios on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blockchain : NSObject
+
+- (NSString *)createWallet;
+
+@end
---
+++ Holla Back/Blockchain.m
@@ -0,0 +1,13 @@
+//
+//  Blockchain.m
+//  Holla Back
+//
+//  Created by Julian Vergel de Dios on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import "Blockchain.h"
+
+@implementation Blockchain
+
+@end
