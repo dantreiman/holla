6ad005da882f35d0903097c550e639e905a86c05
Dan Treiman <dan.treiman@gmail.com>
2014-11-01 17:04:34 -0700

    Enable background modes.  Register settings.

---
+++ Holla Back/NotificationManager.m
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.m
+//  Holla Back
+//
+//  Created by Dan Treiman on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import "NotificationManager.h"
+
+@implementation NotificationManager
+
+@end
