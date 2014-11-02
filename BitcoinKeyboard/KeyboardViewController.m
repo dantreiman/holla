3aa252d941cd35db5c73e5ab0afcf6a3b840e0d6
Dan Treiman <dan.treiman@gmail.com>
2014-11-01 18:34:15 -0700

    First pass at custom keyboard.

---
+++ BitcoinKeyboard/KeyboardViewController.m
@@ -0,0 +1,100 @@
+//
+//  KeyboardViewController.m
+//  BitcoinKeyboard
+//
+//  Created by Dan Treiman on 11/1/14.
+//  Copyright (c) 2014 Only The Best. All rights reserved.
+//
+
+#import "KeyboardViewController.h"
+
+@interface KeyboardViewController ()
+
+
+@property (weak, nonatomic) IBOutlet UISlider * amountSlider;
+@property (weak, nonatomic) IBOutlet UIButton * requestButton;
+
+
+- (IBAction) nextInputMode:(id)sender;
+
+@end
+
+
+@implementation KeyboardViewController
+
+
+- (void)updateViewConstraints {
+    [super updateViewConstraints];
+    
+    // Add custom view sizing constraints here
+}
+
+
+- (void) loadView
+{
+    [super loadView];
+    UINib * nib = [UINib nibWithNibName:@"KeyboardViewController" bundle:nil];
+    [nib instantiateWithOwner:self options:nil];
+    [self amountChanged:self.amountSlider];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated
+}
+
+
+- (void)textWillChange:(id<UITextInput>)textInput {
+    // The app is about to change the document's contents. Perform any preparation here.
+}
+
+
+- (void)textDidChange:(id<UITextInput>)textInput {
+    // The app has just changed the document's contents, the document context has been updated.
+    
+    UIColor *textColor = nil;
+    if (self.textDocumentProxy.keyboardAppearance == UIKeyboardAppearanceDark) {
+        textColor = [UIColor whiteColor];
+    } else {
+        textColor = [UIColor blackColor];
+    }
+}
+
+
+#pragma mark - Actions
+
+
+- (double) BTCAmount
+{
+    // Increment by .002 (about $1 USD)
+    float sliderValue = [self.amountSlider value];
+    double BTC = sliderValue;
+    return BTC;
+}
+
+
+- (IBAction) amountChanged:(id)sender
+{
+    double BTC = [self BTCAmount];
+    double USD = BTC * 350.0; // Approximation
+    NSString * title = [NSString stringWithFormat:@"Request %.3f BTC (%.2f USD)", BTC, USD];
+    [UIView performWithoutAnimation:^{
+        [self.requestButton setTitle:title forState:UIControlStateNormal];
+    }];
+}
+
+
+- (IBAction) requestPayment:(id)sender
+{
+    [self.textDocumentProxy insertText:@"bitcoin:<ADDRESS GOES HERE> 22"];
+}
+
+
+- (IBAction) nextInputMode:(id)sender
+{
+    [self advanceToNextInputMode];
+}
+
+
+@end
